be1946e4d1247237c85b07b7cb354eaa85b00d20
Alice Cieslik <102236373+alicie232@users.noreply.github.com>
2022-07-07 15:03:05 +0200

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,4 @@
+const handler = (request, response) => {
+  return response.json({ message: "Hallo Welt" });
+};
+export default handler;
---
+++ api/users.js
@@ -0,0 +1,4 @@
+const handler = (request, response) => {
+  return response.json({ message: "Ich bin die /users Route" });
+};
+export default handler;
